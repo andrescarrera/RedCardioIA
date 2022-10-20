40f49ed012407a292a36d390cf2f4097dc20fe37
ANDRÉS FELIPE CARRERA <64501465+andrescarrera@users.noreply.github.com>
2022-10-20 13:14:49 -0500

    settings.py changes

--- apps/core/settings.py
+++ apps/core/settings.py
@@ -22,8 +22,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS        = ['168.176.61.6','localhost', 'localhost:85', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+CSRF_TRUSTED_ORIGINS = ['http://168.176.61.6:8000']#,'http://localhost:85', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
 
 # Application definition
 
